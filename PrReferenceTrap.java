701526673325f8b81999152301bead162f455f69
Curt Grogan <curt.grogan@fdmgroup.com>
2022-05-28 01:32:58 -0400

    Tic to the Tac to the Toe GO!

--- PrReferenceTrap.java
+++ PrReferenceTrap.java
@@ -3,10 +3,14 @@ import java.util.Arrays;
 public class PrReferenceTrap {
     public static void main(String[] args) {
         String[] greeting = new String[] { "Hello", "John" };
-        String[] farewell = greeting;
+        String[] farewell = Arrays.copyOf(greeting, greeting.length);
 
         farewell[0] = "Goodbye";
 
+        System.out.println(Arrays.toString(greeting));
+        System.out.println(Arrays.toString(farewell));
+
+        greeting[0] = "Aloha";
         System.out.println(Arrays.toString(greeting));
         System.out.println(Arrays.toString(farewell));
     }
